f1c9d9e3ef37efc83ccb39e9a8cd17c6007fcf6a
Tran Ngoc Khai <khai.tran@ascendcorp.com>
2017-11-01 11:14:40 +0700

    Change script to create redis container

--- Redis/redis_docker.sh
+++ Redis/redis_docker.sh
@@ -1,5 +1,14 @@
+# Remove old `redis` container
+echo "===== Stop old redis container ====="
+docker stop redis
+echo "===== Remove old redis container ====="
+docker rm redis
+
 # Install redis docker and expose port 6379
+echo "===== Install new redis container ====="
 docker run --name redis -p 6379:6379 -d redis
 
 # Run redis-cli from a docker container
-docker run -it --link some-redis:redis --rm redis redis-cli -h redis -p 6379
+echo "===== Run redis-cli in new docker container ====="
+echo "... type \"exit\" to quit"
+docker run -it --link redis:redis --rm redis redis-cli -h redis -p 6379
